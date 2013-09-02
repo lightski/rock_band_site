88175c098d1b461f8f1e747e01fb5a7e836a2502
daleit <leitzke05328@gotoltc.edu>
2013-09-02 15:36:53 -0500

    added index.php

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 rock_band_site
 ==============
 
-website for school project
-the name of our band is "Red Dawn"
+website for school project. 
+the name of our band is "Red Dawn". 
 still working on site specs, etc.
---
+++ index.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+
+<head>	
+	<title>Red Dawn: Home</title>
+	<meta charset="utf-8">
+	<link href="rock_style.css" rel="stylesheet" type="text/css">
+	<script src="jquery-1.9.1.min.js"></script>
+	<script>
+		//javascript or jquery (or both) go here
+	</script>
+</head>
+
+<body>
+
+<?php
+echo "<h1>PHP rocks!</h1>"
+?>
+
+</body>
+
+</html>
