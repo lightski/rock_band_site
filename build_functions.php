71545e9932228541513816c0d145de5d3b435385
dan leitzke <drleitzke@gmail.com>
2013-09-19 23:46:17 -0500

    added image replacement on h1 using css tried to add javascript so that the porthole background moves smoothly from state to state. not sucessful just yet

--- build_functions.php
+++ build_functions.php
@@ -4,18 +4,18 @@
 
 function build_header_nav() {
 	$output = "<header>
-		<h1>Red Dawn</h1>
-		<img id='porthole_front' src='porthole_front.png' width='83px' height='83px' alt='porthole front'>
+		<h1 class='logo'>Red Dawn</h1>
+		<img id='porthole_front' src='porthole_front.png' width='208px' height='208px' alt='porthole front'>
 		<div id='porthole_back'></div>
 		</header>
 		<nav>
 			<ul>
-				<li onmouseover=\"porthole_shift('blog')\"><a href='#'>Blog</a></li>
-				<li onmouseover=\"porthole_shift('about')\"><a href='#'>About</a></li>
-				<li onmouseover=\"porthole_shift('members')\"><a href='#'>Members</a></li>
-				<li onmouseover=\"porthole_shift('store')\"><a href='#'>Store</a></li>
-				<li onmouseover=\"porthole_shift('music')\"><a href='#'>Music</a></li>
-				<li onmouseover=\"porthole_shift('contact')\"><a href='#'>Contact</a></li>
+				<li onmouseover=\"porthole_shift('blog')\" onmouseout=\"record_last('blog')\"><a href='#'>Blog</a></li>
+				<li onmouseover=\"porthole_shift('about')\" onmouseout=\"record_last('about')\"><a href='#'>About</a></li>
+				<li onmouseover=\"porthole_shift('members')\" onmouseout=\"record_last('members')\"><a href='#'>Members</a></li>
+				<li onmouseover=\"porthole_shift('store')\" onmouseout=\"record_last('blog')\"><a href='#'>Store</a></li>
+				<li onmouseover=\"porthole_shift('music')\" onmouseout=\"record_last('blog')\"><a href='#'>Music</a></li>
+				<li onmouseover=\"porthole_shift('contact')\" onmouseout=\"record_last('blog')\"><a href='#'>Contact</a></li>
 			</ul>
 		</nav>";
 	return $output;
--- red_dawn_scripts.js
+++ red_dawn_scripts.js
@@ -1,27 +1,102 @@
+//to do: -fix the setInterval bit at the bottom. 
+//			make it so the porthole back image shifts gradually
+//		 -merge record_last to be the last bit of porthole_shift
+//			that will eliminate need for global
+//		-fix weird layout whitespace gaps (css)
+
+last_region = 0; //getting rid of soon...
+
+function record_last(nav_item){
+	//keeps track of which nav item the cursor just left
+	x_coordinates = new Array();
+	x_coordinates[0] = 0;
+	x_coordinates[1] = 208;
+	x_coordinates[2] = 416;
+	x_coordinates[3] = 624;
+	x_coordinates[4] = 832;
+	x_coordinates[5] = 1024;
+
+	switch(nav_item) {
+		case 'blog':
+			last_region = x_coordinates[0];
+			break;
+		case 'about':
+			last_region = x_coordinates[1];
+			break;
+		case 'members':
+			last_region = x_coordinates[2];
+			break;
+		case 'store':
+			last_region = x_coordinates[3];
+			break;
+		case 'music':
+			last_region = x_coordinates[4]
+			break;
+		case 'contact':
+			last_region = x_coordinates[5];
+			break;
+	}
+}
+
 function porthole_shift(target_region) {
 	//shift the porthole image on mouseover of navigation
-var hover_over = target_region;
-var porthole_back = document.getElementById("porthole_back");
-var back_position = '0px 0px';
-switch(hover_over) {
+	//updated to make the shift more gradual (show off entire graphic)
+
+switch(target_region) {
+	//first, set variable to where the viewport is going
 	case 'blog':
-		back_position = '0px 0px';
+		new_region = 0;
 		break;
 	case 'about':
-		back_position = '-83px 0px';
+		new_region = -208;
 		break;
 	case 'members':
-		back_position = '-166px 0px';
+		new_region = -416;
 		break;
 	case 'store':
-		back_position = '-249px 0px';
+		new_region = -624;
 		break;
 	case 'music':
-		back_position = '-332px 0px';
+		new_region = -832;
 		break;
 	case 'contact':
-		back_position = '-415px 0px';
+		new_region = -1040;
 		break;
 }
-porthole_back.style.backgroundPosition = back_position;
+//vars for the viewport that we're changing and the current location on the image
+var porthole_back = document.getElementById("porthole_back");
+current_region = last_region;
+
+	if (new_region < last_region) {	
+		//while loop decrements
+		//clearTimeout(decrementer); //stop the timer
+			decrementer = setInterval(function(){decrement_image(current_region,new_region)},1000);
+	} else if (new_region > last_region) {
+		//while loop must increment
+		//	clearTimeout(incrementer); //stop the timer
+			incrementer = setInterval(function(){increment_image(current_region, new_region)},1000);
+	}
 }
+
+function decrement_image(current_region, new_region){
+	while (done == false) {
+		current_region -=26;
+		back_position = current_region + "px 0px";
+		porthole_back.style.backgroundPosition = back_position;
+		if (current_region == new_region){
+			done = true;
+		}
+	}
+}
+
+function increment_image(current_region, new_region){
+	while (!done) {
+	current_region +=26;
+	back_position = current_region + "px 0px";
+	porthole_back.style.backgroundPosition = back_position;
+		if (current_region == new_region){
+			done = true;
+		}
+	}
+}
+
